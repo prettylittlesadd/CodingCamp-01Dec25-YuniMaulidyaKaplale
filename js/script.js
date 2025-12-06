dc19d20fdb7fbd2ed85c17bea5e2f0d89a8794ca
prettylittlesadd <ymaulidya66@gmail.com>
2025-12-06 11:22:08 +0800

    tes prompt pop up

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 welcomeMessage();
 
 /// Function to display welcome message
-function showPersonalizedWelcome() {
+// function showPersonalizedWelcome() {
   // 1. Tampilkan Pop-up dan Minta Input Nama
   // Pop-up Anda: "Welcome to Our Website! Input your name:"
-  let userName = prompt("Welcome to Our Website! Input your name:");
-
+  // let userName = prompt("Welcome to Our Website! Input your name:");
+prompt("apakah berfungsi?")
   // 2. Tentukan nama yang akan ditampilkan
   // Jika user menekan Batal (null) atau mengosongkan input (trim() === "")
   if (userName === null || userName.trim() === "") {
